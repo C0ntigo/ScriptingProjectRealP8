7b5dcd2b0dd7862e96063d97473ab9ad5b241d29
Xavier Real <720838@student.dusd.net>
2024-02-28 15:11:05 -0800

    The things I have done
    Destroy Done

--- ScriptingProjectReal/Assets/Scripts/DestroyBasic.cs
+++ ScriptingProjectReal/Assets/Scripts/DestroyBasic.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LinearInterpolation : MonoBehaviour
+public class DestroyBasic : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKey(KeyCode.CapsLock))
+        {
+            Destroy(gameObject);
+        }
     }
 }
