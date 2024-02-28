68e0727a998aea01d898dfb8b7375d6a726a3119
Xavier Real <720838@student.dusd.net>
2024-02-28 15:26:32 -0800

    Did the button scripts
    keyInput and Button Input completed

--- ScriptingProjectReal/Assets/Scripts/ButtonInput.cs
+++ ScriptingProjectReal/Assets/Scripts/ButtonInput.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeyInput : MonoBehaviour
+public class ButtonInput : MonoBehaviour
 {
     public Sprite standard;
     public Sprite downgfx;
@@ -11,15 +11,15 @@ public class KeyInput : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-         
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        bool down = Input.GetKeyDown(KeyCode.Space);
-        bool held = Input.GetKey(KeyCode.Space);
-        bool up = Input.GetKeyUp(KeyCode.Space);
-        
+        bool down = Input.GetButtonDown("Jump");
+        bool held = Input.GetButton("Jump");
+        bool up = Input.GetButtonUp("Jump");
+
     }
 }
